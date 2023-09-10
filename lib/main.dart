89107e2c082e4baa48219e508b276d230a3087bd
snehil <snehilsah400@gmail.com>
2023-09-10 17:45:44 +0530

    potrait view disabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'calculator_screen.dart';
 
 void main() {
+  // add these lines
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
